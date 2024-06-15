3b3476b177f1a96120666df47209a7a8f31221e0
Metaphysiker <s.raess@me.com>
2024-06-15 13:33:35 +0200

    Add example command to pg_dump.sh

--- pg_dump.sh
+++ pg_dump.sh
@@ -1,3 +1,6 @@
 docker exec -t stinah-hufpflege-production-postgres-1 pg_dumpall -c -U abc > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
 
 cat dump_2024-06-15_09_20_48.sql | docker exec -i stinah-hufpflege-production-postgres-1 psql -U abc
+
+cat dump_with_copied_data_from_old.sql | docker exec -i stinah-hufpflege-production-postgres-1 psql -U stinah-hufpflege-user stinah-hufpflege-db
+
