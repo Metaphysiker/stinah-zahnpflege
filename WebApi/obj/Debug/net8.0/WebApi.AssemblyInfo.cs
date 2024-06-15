d6d51097fd88462a441363d7609705a801aa7952
Metaphysiker <s.raess@me.com>
2024-06-15 12:22:11 +0200

    Add dump.

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2ac29c130261165bc5b690ac102a2ddc789e16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6672dbd98acf84a0f7b04f49ae930e92897489eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
