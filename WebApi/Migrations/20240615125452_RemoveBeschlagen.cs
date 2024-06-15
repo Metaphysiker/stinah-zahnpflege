3955f0797c520a8dc6c4b1ba62816cd3547d6a6b
Metaphysiker <s.raess@me.com>
2024-06-15 14:57:30 +0200

    Add migration to remove beschlagen.

---
+++ WebApi/Migrations/20240615125452_RemoveBeschlagen.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBeschlagen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Beschlagen",
+                table: "Horses");
+
+            migrationBuilder.DropColumn(
+                name: "BirthYear",
+                table: "Horses");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Beschlagen",
+                table: "Horses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BirthYear",
+                table: "Horses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
