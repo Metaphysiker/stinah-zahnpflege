5fcc71baa82fae4f30b98a01c1554a2fdfd464b5
Metaphysiker <s.raess@me.com>
2024-06-22 00:09:38 +0200

    Update pg_dump.sh

--- pg_dump.sh
+++ pg_dump.sh
@@ -1,5 +1,8 @@
 docker exec -t stinah-zahnpflege-production-postgres-1 pg_dumpall -c -U abc > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
 
+docker exec -t stinah-zahnpflege-production-postgres-1 pg_dumpall -c -U stinahzahnpflegeuser > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
+
+
 cat dump_2024-06-15_09_20_48.sql | docker exec -i stinah-zahnpflege-production-postgres-1 psql -U abc
 
 cat dump_with_copied_data_from_old.sql | docker exec -i stinah-zahnpflege-production-postgres-1 psql -U stinah-zahnpflege-user stinah-zahnpflege-db
